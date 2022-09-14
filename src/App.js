2959eab9fd9e6679fdbadc639fe911d47d79e6e0
yang <joey674901205@gmail.com>
2022-09-14 16:47:45 -0400

    add contact component, contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './component/Header';
 import About from './component/About';
 import Portfolio from './component/Portfolio';
+import ContactForm from './component/Contact';
 import Footer from './component/Footer';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main>
         <About></About>
         <Portfolio></Portfolio>
+        <ContactForm></ContactForm>
       </main>
       <Footer></Footer>
     </div>
