a034d536e18b05b70907119e82c7d3c8ecd25617
yang <joey674901205@gmail.com>
2022-09-20 21:14:26 -0400

    add contact form style

--- src/component/Contact/index.js
+++ src/component/Contact/index.js
@@ -1,36 +1,40 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../../utils/helpers';
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  const {name, email, message } = formState;
+  const { name, email, message } = formState;
 
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleChange(e) {
-    if (e.target.name === 'email') {
+    if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
       console.log(isValid);
       // isValid conditional statement
       if (!isValid) {
-        setErrorMessage('Your email is invalid.');
+        setErrorMessage("Your email is invalid.");
       } else {
-        setErrorMessage('');
-      } 
+        setErrorMessage("");
+      }
     } else {
       if (!e.target.value.length) {
         setErrorMessage(`${e.target.name} is required.`);
       } else {
-        setErrorMessage('');
+        setErrorMessage("");
       }
     }
 
     if (!errorMessage) {
-      setFormState({...formState, [e.target.name]: e.target.value })
+      setFormState({ ...formState, [e.target.name]: e.target.value });
     }
 
-    console.log('errorMessage', errorMessage);
+    console.log("errorMessage", errorMessage);
   }
 
   function handleSubmit(e) {
@@ -41,28 +45,66 @@ function ContactForm() {
   return (
     <section>
       <h2>Contact me</h2>
-      <form id='contact-form' onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='name'>Name:</label>
-          <input type='text' defaultValue={name} onBlur={handleChange} name='name' />
-        </div>
-        <div>
-          <label htmlFor='email'>Email address:</label>
-          <input type='email' defaultValue={email} name='email' onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor='message'>Message:</label>
-          <textarea name='message' defaultValue={message} onBlur={handleChange} rows='5' />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className='error-text'>{errorMessage}</p>
+      <form
+        id="contact-form"
+        className="mt-4 border border-light border-4 rounded-4 p-3 mx-5 d-flex justify-content-evenly"
+        onSubmit={handleSubmit}
+      >
+        <div class="border border-light border-4 rounded-4 p-3 flex-grow-1 me-2">
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name:
+            </label>
+            <input
+              type="text"
+              defaultValue={name}
+              onBlur={handleChange}
+              name="name"
+              className="form-control"
+            />
           </div>
-        )}
-        <button data-testid='buttontag' type='submit'>Submit</button>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address:
+            </label>
+            <input
+              type="email"
+              defaultValue={email}
+              name="email"
+              onBlur={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">
+              Message:
+            </label>
+            <textarea
+              name="message"
+              defaultValue={message}
+              onBlur={handleChange}
+              rows="5"
+              className="form-control"
+            />
+          </div>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+
+        <div className="border border-light border-4 rounded-4 p-3">
+          <h4>My contact information</h4>
+          <email>Email: joey674901205@gmail.com</email>
+          <p>Phone: 674901205</p>
+        </div>
       </form>
     </section>
-  )
+  );
 }
 
 export default ContactForm;
